de57257449d7cc4c1e1f1ab253719363f993c1c2
kti0940 <kti0940@gmail.com>
2022-05-18 08:34:25 +0900

    turtle prac2

--- api.js
+++ api.js
@@ -1,13 +1,34 @@
+const backend_base_url = "http://127.0.0.1:5000"
+const frontend_base_url = "http://127.0.0.1:5500"
+
 async function handleSignin() {
     const signupData = {
         email: document.getElementById("floatingInput").value,
         password: document.getElementById("floatingPassword").value
     }
 
-    const response = await fetch('http://127.0.0.1:5000/signup', {
+    const response = await fetch(`${backend_base_url}/signup`, {
         method: 'POST',
         body: JSON.stringify(signupData)
     }
     )
     console.log(response)
+}
+
+async function handleLogin() {
+    const signupData = {
+        email: document.getElementById("floatingInput").value,
+        password: document.getElementById("floatingPassword").value
+    }
+
+    const response = await fetch(`${backend_base_url}/login`, {
+        method: 'POST',
+        body: JSON.stringify(signupData)
+    }
+    )
+    console.log(response)
+
+    response_json = await response.json()
+    console.log(response_json)
+    localStorage.setItem("token", response_json.token)
 }
