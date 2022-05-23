b499ca56ecbfd20f68579352df13edc33ba06fe3
taein Kim <kti0940@gmail.com>
2022-05-23 21:19:18 +0900

    백엔드 4-10 완료

--- app.py
+++ app.py
@@ -1,44 +1,78 @@
-from dataclasses import dataclass
-import json
+from datetime import datetime, timedelta
+from functools import wraps
 import hashlib
-import jwt
-
-from flask import Flask, jsonify, request  # 플라스크 서버 사용 필요한 기능들 임포트
+import json
+from bson import ObjectId
+from flask import Flask, abort, jsonify, request, Response
 from flask_cors import CORS
+import jwt
 from pymongo import MongoClient
+# from bson.json_util import loads, dumps
 
-app = Flask(__name__)  # Flask 실행?
+SECRET_KEY = 'turtle'
+
+
+app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
-
 client = MongoClient('localhost', 27017)
 db = client.turtlegram
 
 
-@app.route("/")  # url 경로 지정
-def hello_world():  # hello_world 로 함수 지정
-    return jsonify({'message': 'success'})  # 결과값은 제이슨 형식으로 키:벨류를 돌려줌
+def authorize(f):
+    @wraps(f)
+    def decorated_function():
+        if not 'Authorization' in request.headers:
+            abort(401)
+        token = request.headers['Authorization']
+        print(token)
+        try:
+            user = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except:
+            abort(401)
+        return f(user)
+    return decorated_function
+
+
+@app.route("/")
+@authorize
+def hello_world(user):
+    print(user)
+    return jsonify({'message': 'success'})
 
 
 @app.route("/signup", methods=["POST"])
 def sign_up():
     data = json.loads(request.data)
+    # data = loads(request.data)
+    print(data.get('email'))
+    print(data["password"])
 
-    email = data.get('email')
-    password = data.get('password')
+    # 이메일 중복시 에러처리
 
-    print(email, password)
-
-    pw_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    # 비밀번호 해싱
+    pw = data.get('password', None)
+    hashed_password = hashlib.sha256(pw.encode('utf-8')).hexdigest()
 
     doc = {
-        'email': email,
-        'password': pw_hash
+        'email': data.get('email'),
+        'password': hashed_password
     }
 
-    db.turtleuser.insert_one(doc)
+    # doc = {
+    #     'email': data.get('email'),
+    #     'password': data.get('password')
+    # }
+
+    print(doc)
+    user = db.users.insert_one(doc)
     print(doc)
 
-    return jsonify({'status': 'success'})
+    return jsonify({"status": "success"})
+
+    # # dump 사용
+    # json_doc = dumps(doc)
+    # print(json_doc)
+    # return json_doc
 
 
 @app.route("/login", methods=["POST"])
@@ -47,26 +81,23 @@ def login():
     data = json.loads(request.data)
     print(data)
 
-    return jsonify({'status': 'success'})
-
     email = data.get("email")
     password = data.get("password")
-    pw_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    print(pw_hash)
+    hashed_pw = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    print(hashed_pw)
 
     result = db.users.find_one({
         'email': email,
-        'password': pw_hash
+        'password': hashed_pw
     })
-
     print(result)
 
     if result is None:
-        return jsonify({"message": "아이디나 비밀번호가 올바르지 않습니다"}), 401
+        return jsonify({"message": "아이디나 비밀번호가 옳지 않습니다."}), 401
 
     payload = {
         'id': str(result["_id"]),
-        'exp': datetime.utcnow() + timedelta(second=60 * 60 * 24)
+        'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
     }
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
     print(token)
@@ -75,12 +106,49 @@ def login():
 
 
 @app.route("/getuserinfo", methods=["GET"])
-def get_user_info():
-    token = request.headers.get("Autorization")
-    print(token)
+@authorize
+def get_user_info(user):
+    result = db.users.find_one({
+        '_id': ObjectId(user["id"])
+    })
+
+    return jsonify({"message": "success", "email": result["email"]})
+
+
+@app.route("/article", methods=["POST"])
+@authorize
+def post_article(user):
+    data = json.loads(request.data)
+
+    db_user = db.users.find_one({'_id': ObjectId(user.get('id'))})
+
+    # print(db_user)
+
+    now = datetime.now().strftime("%H:%M:%S")
+
+    doc = {
+        'title': data.get('title', None),
+        'content': data.get('content', None),
+        'user': user['id'],
+        'user_email': db_user['email'],
+        'time': now,
+    }
+
+    print(doc)
+
+    db.article.insert_one(doc)
 
     return jsonify({"message": "success"})
 
 
+@app.route("/article", methods=["GET"])
+def get_article():
+    articles = list(db.article.find())
+    for article in articles:
+        article['_id'] = str(article['_id'])
+
+    return jsonify({"message": "success", "articles": articles})
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5002, debug=True)
