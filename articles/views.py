eb67eaa572ab0016ce6594095d888796a1779ada
taein Kim <kti0940@gmail.com>
2022-05-31 19:45:20 +0900

    3-5까지 완료

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,16 @@
+import random
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request, name):
+    menus = [{"name":'족발', "price": 30000}, {"name":'햄버거', "price": 10000}, {"name":'치킨', "price": 20000}, {"name":'초밥', "price": 18000},]
+    pick =random.choice(menus)
+    context = {
+        'pick': pick,
+        'name': name,
+        'menus': menus,
+    }
+    return render(request, 'dinner.html', context)
