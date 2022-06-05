b0218c695dc5e0d1b63ab49224ecf39e9aa438b3
taein Kim <kti0940@gmail.com>
2022-06-05 17:50:42 +0900

    6-7 완료

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 import random
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
 # Create your views here.
 def index(request):
@@ -8,10 +9,12 @@ def index(request):
 def dinner(request, name):
     menus = [{"name":'족발', "price": 30000}, {"name":'햄버거', "price": 10000}, {"name":'치킨', "price": 20000}, {"name":'초밥', "price": 18000},]
     pick =random.choice(menus)
+    articles = Article.objects.order_by('-pk')
     context = {
         'pick': pick,
         'name': name,
         'menus': menus,
+        'articles':articles,
     }
     return render(request, 'dinner.html', context)
 
@@ -20,8 +23,9 @@ def review(request):
 
 def create_review(request):
     content = request.POST.get('content')
-    print(request.POST)
-    context = {
-        'content':content,
-    }
-    return render (request, 'review_result.html', context)
+    title = request.POST.get('title')
+    
+    article = Article(title=title, content=content)
+    article.save()
+    
+    return redirect ('/articles/dinner/무언가/')
