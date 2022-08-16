d0ef84dae6c60b8b550f944dc5aa4d46aac723ea
taein Kim <kti0940@gmail.com>
2022-08-16 10:39:34 +0900

    remove file

--- timeattack7-8/lasttimeattack/product/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Product)
-admin.site.register(models.Subscribe)
--- timeattack7-8/lasttimeattack/product/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    path('', views.ProductView.as_view(), name='product_view'),
-]
